d131bda2df3345c7c2a559be8e36ce11c35f240d
victorpellicci <victor1994@hotmail.com.br>
2021-11-05 14:30:37 -0300

    1.0

--- model.py
+++ model.py
@@ -28,6 +28,21 @@ class Model(Donaclotilde):
         #print(sql)
         self.insert(sql)
 
+    def update(self,kwargs):
+        
+        valores=[]
+        for x in kwargs.values():
+            valores.append(x)
+
+        colunas=[]
+        for x in kwargs.keys():
+            colunas.append(x)
+
+        self.where(kwargs['id'],"id","=")
+
+        sql = self.set("usuarios",valores,colunas)
+        #print(sql)
+        self.insert(sql)
     def listall(self):
         
         self.select('id') #indice 0 
@@ -45,14 +60,19 @@ class Model(Donaclotilde):
         
 
         self.select('id') #indice 0 
-        self.select('nome_cedente')#indice 1
-        self.select('nome_banco')#indice 2
-        self.select('nome_agencia')#indice 3
-        self.select('nome_conta')#indice 4
+        self.select('usuario')#indice 1
+        self.select('banco')#indice 2
+        self.select('agencia')#indice 3
+        self.select('conta')#indice 4
+        self.select('CPF')#indice 5
+        self.select('CEP')#indice 6
+        self.select('endereco')#indice 7
+        self.select('cidade')#indice 8
+        self.select('estado')#indice 9
 
         self.from_table("usuarios")
         if search:
-            self.where(search,"nome_cedente")
+            self.where(search,"usuario")
             
         sql = self.get()
         data = self.result_list(sql)
--- vapboletos.py
+++ vapboletos.py
@@ -4,8 +4,7 @@ import pygubu
 import sys
 from ttkbootstrap import Style
 from model import Model
-
-
+import time
 
 style = Style(theme='classic')
 
@@ -25,13 +24,13 @@ class VapboletosApp:
         self.combobox1 = builder.get_object('combobox1')
         self.combobox1['values'] = ['aucelio', 'richard', 'fran']
 
-        # widgets da aba cadastro lado esquerdo
+        # widgets da aba incluir lado esquerdo
         self.entry1 = builder.get_object('entry1')
         self.entry21 = builder.get_object('entry21')
         self.entry10 = builder.get_object('entry10')
         self.entry11 = builder.get_object('entry11')
 
-        # widgets da aba cadastro lado direito
+        # widgets da aba incluir lado direito
         self.entry12 = builder.get_object('entry12')
         self.entry14 = builder.get_object('entry14')
         self.entry13 = builder.get_object('entry13')
@@ -39,23 +38,91 @@ class VapboletosApp:
         self.entry16 = builder.get_object('entry16')
         self.entry17 = builder.get_object('entry17')
 
+        # widgets da aba editar lado esquerdo
+        self.combobox5 = builder.get_object('combobox5')
+        self.combobox5['values'] = ['aucelio', 'richard', 'fran']
+        self.entry18 = builder.get_object('entry18')
+        self.entry22 = builder.get_object('entry22')
+        self.entry19 = builder.get_object('entry19')
+        self.entry20 = builder.get_object('entry20')
+        self.label35 = builder.get_object('label35')
+
+        # widgets da aba editar lado direito
+        self.combobox6 = builder.get_object('combobox6')
+        self.combobox6['values'] = [['aucelio','conta1'], ['richard','conta2'], ['fran',"conta3"]]
+        self.entry23 = builder.get_object('entry23')
+        self.entry28 = builder.get_object('entry28')
+        self.entry24 = builder.get_object('entry24')
+        self.entry25 = builder.get_object('entry25')
+        self.entry26 = builder.get_object('entry26')
+        self.entry27 = builder.get_object('entry27')
+
+        # widgets da aba criar cobranca lado esquerdo
+        self.spinbox1 = builder.get_object('spinbox1')
+        dia = []
+        x=1
+        while x<32:
+            dia.append(x)
+            x=x+1  
+        self.spinbox1['values'] = dia
+        self.spinbox2 = builder.get_object('spinbox2')
+        mes = []
+        x=1
+        while x<13:
+            mes.append(x)
+            x=x+1  
+        self.spinbox2['values'] = mes
+        self.spinbox3 = builder.get_object('spinbox3')
+        ano = []
+        x=2021
+        while x<2026:
+            ano.append(x)
+            x=x+1  
+        self.spinbox3['values'] = ano
+
+        #widgets da aba criar cobranca lado direito
+        self.spinbox4 = builder.get_object('spinbox4')
+        dia = []
+        x=1
+        while x<32:
+            dia.append(x)
+            x=x+1  
+        self.spinbox4['values'] = dia
+        self.spinbox5 = builder.get_object('spinbox5')
+        mes = []
+        x=1
+        while x<13:
+            mes.append(x)
+            x=x+1  
+        self.spinbox5['values'] = mes
+        self.spinbox6 = builder.get_object('spinbox6')
+        ano = []
+        x=2021
+        while x<2026:
+            ano.append(x)
+            x=x+1  
+        self.spinbox6['values'] = ano
+
         self.madruga = Model()
 
     def run(self):
         self.mainwindow.mainloop()
 
-
-
-
     def cadastrarcedente(self):
 
         dados={}
+
         dados['usuario'] = self.entry1.get()
         dados['banco'] = self.entry21.get()
         dados['agencia'] = self.entry10.get()
         dados['conta'] = self.entry11.get()
         print(dados)
         self.madruga.save(dados)
+        self.entry1.delete(0, 'end')
+        self.entry21.delete(0, 'end')
+        self.entry10.delete(0, 'end')
+        self.entry11.delete(0, 'end')
+
 
     def cadastrarsacado(self):
 
@@ -68,12 +135,34 @@ class VapboletosApp:
         dados['estado'] = self.entry17.get()
         print(dados)
         self.madruga.save(dados)
+        self.entry12.delete(0, 'end')
+        self.entry14.delete(0, 'end')
+        self.entry13.delete(0, 'end')
+        self.entry15.delete(0, 'end')
+        self.entry16.delete(0, 'end')
+        self.entry17.delete(0, 'end')
+
+    def update(self):
+        pass
+
+    def editarcedente(self):
+        self.label35.config(text='Cedente editado!')
+        self.label35.after(3000, self.label35.forget())
+
+
+    def editarsacado(self):
+        pass
 
 
     def exibir_dados(self):
-
+        # apenas para teste, apagar depois
         dados = self.madruga.listall()
         print(dados[1][0])
+        print(dados[1][1])
+        print(dados[0][0])
+        print(dados[0][1])
+        print(dados[1])
+        print(dados[0])
 
 
     def exibir_dados_filtro(self):
@@ -83,10 +172,6 @@ class VapboletosApp:
         
         dados = self.madruga.list_filt(busca)
         print(dados)
-
-
-
-
 
 
 if __name__ == '__main__':
